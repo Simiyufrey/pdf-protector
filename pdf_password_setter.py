8cadcda61b72607ea4e7227e3672dd40e323e8ed
Simiyufrey <simiyufrey@gmail.com>
2023-08-23 21:34:44 -0400

    Added 2 files README and pdf_password_setter

---
+++ pdf_password_setter.py
@@ -0,0 +1,22 @@
+import PyPDF2
+
+class Pdf_protector():
+    
+    def __init__(self,file_path,password):
+        self.file_path=file_path
+        self.password=password 
+    def protect(self):
+        text_content = []
+        pdf_writer=PyPDF2.PdfWriter()
+        with open(self.file_path, 'rb') as pdf_file:
+            pdf_reader = PyPDF2.PdfReader(pdf_file)
+            for page_num in range(len(pdf_reader.pages)):
+                page = pdf_reader.pages[page_num]
+                pdf_writer.add_page(page)
+            pdf_file.close()
+            
+        with open(self.file_path,"wb") as f2:
+            pdf_writer.encrypt(self.password)
+            pdf_writer.write(f2)
+if __name__ =="__main__":
+    Pdf_protector("Cover letter.pdf","Password").protect()
